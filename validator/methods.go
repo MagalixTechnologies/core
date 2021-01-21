44e366777295f46229f4bee1ff0b5a07b763fb83
Khaled K. Badr <khaledkbadr@outlook.com>
2021-01-21 18:20:06 +0200

    support translations directly
    Signed-off-by: Khaled K. Badr <khaledkbadr@outlook.com>

---
+++ validator/methods.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"context"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// StructCtx validates a structs exposed fields, and automatically validates nested structs, unless otherwise specified
+// and also allows passing of context.Context for contextual validation information.
+//
+// It returns InvalidValidationError for bad values passed in and nil or ValidationErrors as error otherwise.
+// You will need to assert the error if it's not nil eg. err.(validator.ValidationErrors) to access the array of errors.
+func StructCtx(ctx context.Context, s interface{}) interface{} {
+	err := Validator.StructCtx(ctx, s)
+	if err == nil {
+		return nil
+	}
+
+	validationErrs := err.(validator.ValidationErrors)
+	return validationErrs.Translate(Translator)
+}
