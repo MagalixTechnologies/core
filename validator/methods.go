9f57815742341ce110a57dd899c2020be956a270
Khaled K. Badr <khaledkbadr@outlook.com>
2021-02-03 02:32:49 +0200

    add more validator wrappers
    Signed-off-by: Khaled K. Badr <khaledkbadr@outlook.com>

--- validator/methods.go
+++ validator/methods.go
@@ -34,3 +34,34 @@ func Struct(s interface{}) interface{} {
 	validationErrs := err.(validator.ValidationErrors)
 	return validationErrs.Translate(Translator)
 }
+
+// StructExcept validates all fields except the ones passed in.
+// Fields may be provided in a namespaced fashion relative to the  struct provided
+// i.e. NestedStruct.Field or NestedArrayField[0].Struct.Name
+//
+// It returns InvalidValidationError for bad values passed in and nil or ValidationErrors as error otherwise.
+// You will need to assert the error if it's not nil eg. err.(validator.ValidationErrors) to access the array of errors.
+func StructExcept(s interface{}, fields ...string) interface{} {
+	err := Validator.StructExcept(s, fields...)
+	if err == nil {
+		return nil
+	}
+	validationErrs := err.(validator.ValidationErrors)
+	return validationErrs.Translate(Translator)
+}
+
+// StructExceptCtx validates all fields except the ones passed in and allows passing of contextual
+// validation validation information via context.Context
+// Fields may be provided in a namespaced fashion relative to the  struct provided
+// i.e. NestedStruct.Field or NestedArrayField[0].Struct.Name
+//
+// It returns InvalidValidationError for bad values passed in and nil or ValidationErrors as error otherwise.
+// You will need to assert the error if it's not nil eg. err.(validator.ValidationErrors) to access the array of errors.
+func StructExceptCtx(ctx context.Context, s interface{}, fields ...string) interface{} {
+	err := Validator.StructExceptCtx(ctx, s, fields...)
+	if err == nil {
+		return nil
+	}
+	validationErrs := err.(validator.ValidationErrors)
+	return validationErrs.Translate(Translator)
+}
