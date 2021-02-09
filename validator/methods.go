0e4cff60738625ff4eacf5485ec9fe9954e98646
ahsayde <ahmed.m.elsayed93@gmail.com>
2021-02-09 17:11:27 +0200

    return vaildation errors as an array of strings

--- validator/methods.go
+++ validator/methods.go
@@ -11,28 +11,42 @@ import (
 //
 // It returns InvalidValidationError for bad values passed in and nil or ValidationErrors as error otherwise.
 // You will need to assert the error if it's not nil eg. err.(validator.ValidationErrors) to access the array of errors.
-func StructCtx(ctx context.Context, s interface{}) interface{} {
+func StructCtx(ctx context.Context, s interface{}) []string {
 	err := Validator.StructCtx(ctx, s)
 	if err == nil {
 		return nil
 	}
 
 	validationErrs := err.(validator.ValidationErrors)
-	return validationErrs.Translate(Translator)
+	errsMap := validationErrs.Translate(Translator)
+
+	errs := make([]string, 0, len(errsMap))
+	for _, v := range errsMap {
+		errs = append(errs, v)
+	}
+
+	return errs
 }
 
 // Struct validates a structs exposed fields, and automatically validates nested structs, unless otherwise specified.
 //
 // It returns InvalidValidationError for bad values passed in and nil or ValidationErrors as error otherwise.
 // You will need to assert the error if it's not nil eg. err.(validator.ValidationErrors) to access the array of errors.
-func Struct(s interface{}) interface{} {
+func Struct(s interface{}) []string {
 	err := Validator.Struct(s)
 	if err == nil {
 		return nil
 	}
 
 	validationErrs := err.(validator.ValidationErrors)
-	return validationErrs.Translate(Translator)
+	errsMap := validationErrs.Translate(Translator)
+
+	errs := make([]string, 0, len(errsMap))
+	for _, v := range errsMap {
+		errs = append(errs, v)
+	}
+
+	return errs
 }
 
 // StructExcept validates all fields except the ones passed in.
